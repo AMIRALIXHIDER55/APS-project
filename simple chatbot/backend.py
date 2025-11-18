a5d867b54828244d51bc4a0347cb5b4b8c43b054
AMIRALIXHIDER55 <amirali764209@gmail.com>
2025-11-18 21:18:27 +0330

    Update backend.py adding shield for undrestanding

--- simple chatbot/backend.py
+++ simple chatbot/backend.py
@@ -8,15 +8,38 @@ CORS(app)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="")
 
 @app.route('/')
 def home():
-    return send_from_directory(BASE_DIR, 'bac.html')
+    return send_from_directory(BASE_DIR, 'frontend.html')
+
+def classify(message):
+    prompt = f"""
+    پیام زیر را از نظر نوع دسته‌بندی کن. فقط یکی از این موارد را برگردان:
+    - پرسش عمومی
+    - تشخیص بیماری
+    -پیدا کردن بیمارستان
+    -رزرو وقت
+    -پیدا کردن داروخانه و دارو
+
+    پیام:
+    {message}
+
+    فقط نوع پیام را برگردان.
+    """
+
+    classification = client.responses.create(
+        model="gpt-5-nano", 
+        input=prompt
+    )
+
+    return classification.output_text.strip()
 
 @app.route('/chat', methods=['POST'])
 def chat():
     user_message = request.json.get('message', '')
+    print(classify(user_message))
     if not user_message:
         return jsonify({'error': 'پیام خالی است'}), 400
 
@@ -24,8 +47,9 @@ def chat():
         model="gpt-5-nano",
         input=user_message
     )
-
     return jsonify({'reply': response.output_text.strip()})
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
+
